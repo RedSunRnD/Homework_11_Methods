13a24bd89958840fab1aa117d949761add064ae8
RedSunRnD <vvatutin.rs@gmail.com>
2024-09-19 22:03:24 +0300

    Corrected homework

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,34 @@
 import java.time.LocalDate;
 
 public class Main {
+
+
+    public static void main(String[] args) {
+
+        //Задача №1
+        int year = 2001;
+        checkLeapYear(year);
+        System.out.println();
+
+        //Задача №2
+        int clientOS = 0;
+        int clientDeviceYear = 2023;
+        downloadCorrectOS(clientOS, clientDeviceYear);
+        System.out.println();
+
+        //Задача №3
+        int deliveryDistance = -5;
+        if (deliveryDistance < 0) {
+            throw new RuntimeException("Неверно введено расстояние.");
+        }
+        int deliveryTime = calculateDeliveryTime(deliveryDistance);
+        if (deliveryTime == 0) {
+            System.out.println("Доставки нет.");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        }
+    }
+
     public static void checkLeapYear(int yearForChecking) {
         if (yearForChecking >= 1584 && ((yearForChecking % 400 == 0) || (yearForChecking % 100 != 0 && yearForChecking % 4 == 0))) {
             System.out.println(yearForChecking + " год является високосным.");
@@ -22,33 +50,16 @@ public class Main {
         }
     }
 
-    public static void calculateDeliveryTime(int deliveryDistance) {
-        if (deliveryDistance <= 20) {
-            System.out.println("На доставку потребуется 1 день");
+    public static int calculateDeliveryTime(int deliveryDistance) {
+        if (deliveryDistance <= 20 && deliveryDistance > 0) {
+            deliveryDistance = 1;
         } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("На доставку потребуется 2 дня");
+            deliveryDistance = 2;
         } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("На доставку потребуется 3 дня");
+            deliveryDistance = 3;
         } else if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
+            deliveryDistance = 0;
         }
-    }
-
-    public static void main(String[] args) {
-
-        //Задача №1
-        int year = 2001;
-        checkLeapYear(year);
-        System.out.println();
-
-        //Задача №2
-        int clientOS = 0;
-        int clientDeviceYear = 2023;
-        downloadCorrectOS(clientOS, clientDeviceYear);
-        System.out.println();
-
-        //Задача №3
-        int deliveryDistance = 104;
-        calculateDeliveryTime(deliveryDistance);
+        return deliveryDistance;
     }
 }
